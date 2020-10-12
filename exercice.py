b0d037b7bc739981d83c4fdc8d65592f7210a6b1
ghalich <ghali.chraibi@polymtl.ca>
2020-10-12 14:01:09 +0000

    Auto commit

--- exercice.py
+++ exercice.py
@@ -70,7 +70,7 @@ def frequence(sentence: str) -> dict:
 
     while True:
         highest = max(occ_dict, key=occ_dict.get)
-        if occ_dict
+        if occ_dict[highest] <= 5
         five_highest.append(highest)
         del occ_dict[highest]
         
